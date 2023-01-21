4c720a683aaf9a52e9a30ab0214880696c3b4bc6
hiteshkumar794 <hiteshkumar794@gmail.com>
2023-01-22 02:36:50 +0530

    BinarySearch Program Added

---
+++ AlgorithmProgrammingProblems/BinarySearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AlgorithmProgrammingProblems
+{
+    public class BinarySearch
+    {
+        public void BinarySearchFn()
+        {
+            //Initialised an array
+            int[] arrayData = { 12, 22, 38, 56, 64, 78, 84, 89, 93, 98 };
+            Console.WriteLine("Array Data is  :");
+            //Print Array
+            foreach (int i in arrayData)
+            {
+                Console.Write(i + " ");
+            }
+            //Choose Number to check position
+            Console.WriteLine("\nChoose a Number to find the index position");
+            int searchItem = Convert.ToInt32(Console.ReadLine());
+            int start = 0;
+            int end = arrayData.Length - 1; //9
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (searchItem < arrayData[mid])
+                {
+                    end = mid - 1;
+                }
+                else if (searchItem == arrayData[mid])
+                {
+                    Console.WriteLine("Index Position is : " + mid);
+                    break;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+        }
+    }
+}
--- AlgorithmProgrammingProblems/Program.cs
+++ AlgorithmProgrammingProblems/Program.cs
@@ -11,13 +11,17 @@ namespace AlgorithmProgrammingProblems
         static void Main(string[] args)
         {
             Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\nWelcome to Algorithm Problems\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-            Console.WriteLine("1.Anagram.");
+            Console.WriteLine("1.Anagram.\n2.Binary Search.\n");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1: //Anagram Detection 
                     Anagram anagram = new Anagram();
                     anagram.CheckAnagram();
+                    break;
+                case 2: //Binary Search 
+                    BinarySearch binarySearch = new BinarySearch();
+                    binarySearch.BinarySearchFn();
                     break;
                 default:
                     Console.WriteLine("Invalid Option");
